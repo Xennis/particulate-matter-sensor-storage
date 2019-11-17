c2d79b4f8dff29d6e79cf032e0fe7ab31b316e43
Xennis <code@xennis.org>
2019-11-17 15:19:11 +0100

    Store data in BigQuery

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import typing
 from datetime import datetime
 
 from flask import Request, Response
-from google.cloud import firestore
+from google.api_core.exceptions import NotFound
+from google.cloud import firestore, bigquery
 
 
 class RequestInvalidError(Exception):
@@ -36,6 +37,30 @@ def extract_sensor_data(data: typing.Optional[dict]) -> typing.Dict[str, typing.
     return r
 
 
+def get_or_create_table(client: bigquery.Client) -> bigquery.Table:
+    try:
+        dataset = client.get_dataset('pm_sensor')
+    except NotFound as _:
+        dataset = client.create_dataset('pm_sensor')
+
+    # The default project ID is not set and hence a fully-qualified ID is required.
+    table_ref = bigquery.TableReference(dataset, table_id='data')
+    try:
+        return client.get_table(table_ref)
+    except NotFound as _:
+        return client.create_table(bigquery.Table(table_ref, schema=[
+            bigquery.SchemaField('humidity', 'NUMERIC', description='Sensor DHT22humidity in %'),
+            bigquery.SchemaField('max_micro', 'NUMERIC', description=''),
+            bigquery.SchemaField('min_micro', 'NUMERIC', description=''),
+            bigquery.SchemaField('samples', 'NUMERIC', description=''),
+            bigquery.SchemaField('sds_p1', 'NUMERIC', description='Sensor SDS011 PM10 in µg/m³'),
+            bigquery.SchemaField('sds_p2', 'NUMERIC', description='Sensor PM2.5 PM10 in µg/m³'),
+            bigquery.SchemaField('signal', 'NUMERIC', description='WiFi signal strength in dBm'),
+            bigquery.SchemaField('temperature', 'NUMERIC', description='Sensor DHT22 temperature in °C'),
+            bigquery.SchemaField('datetime', 'DATETIME', description='Datetime of measurement', mode='REQUIRED'),
+        ]))
+
+
 def pm_sensor_storage(request: Request) -> Response:
     """HTTP Cloud Function.
     Args:
@@ -52,9 +77,16 @@ def pm_sensor_storage(request: Request) -> Response:
         logging.warning('invalid request: %s', e)
         return Response(status=400)
 
-    sensor_data['timestamp'] = datetime.now()
+    sensor_data['datetime'] = datetime.now()
 
     db = firestore.Client()
     doc_ref = db.collection('pm_sensor')
     doc_ref.add(sensor_data)
+
+    client = bigquery.Client()
+    errors = client.insert_rows(get_or_create_table(client), [sensor_data])
+    if errors:
+        logging.warning('failed to store data in bigquery: %s', errors)
+        return Response(status=500)
+
     return Response(status=201)
